5cf2e0dae203ea343d9e922a8a17f885cf28380d
pawan.rastogi <pawan.rastogi@BDC6-LX-6WKZRQ2.dir.svc.accenture.com>
2020-01-20 16:04:43 +0530

    First Time Pawan Done

---
+++ src/test/java/p1/DevOpsTest.java
@@ -0,0 +1,67 @@
+package p1;
+
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+
+ 
+
+/**
+ * @author pawan.rastogi
+ *  This is a first ever test case written by Pawan using Vishnu help to check how the devops works.
+ *  the date is 20 jan 2020
+ *
+ */
+public class DevOpsTest {
+    WebDriver driver;
+    @Test
+    public void openBrowser() throws InterruptedException {
+        driver.get("https://login.yahoo.com/account/create?specId=yidReg");    
+        
+        
+        driver.findElement(By.name("firstName")).sendKeys("John");
+        
+        driver.findElement(By.id("usernamereg-lastName")).sendKeys("doe");
+        
+        driver.findElement(By.name("yid")).sendKeys("abcde@yahoo.com");
+        
+        driver.findElement(By.className("phone-no")).sendKeys("9858965667");
+        
+        Select s= new Select(driver.findElement(By.name("mm")));
+        s.selectByVisibleText("March");
+        
+        Thread.sleep(2000);
+        s.selectByIndex(9);
+        Thread.sleep(2000);
+        s.selectByValue("7");
+        
+    
+        
+    }
+
+ 
+
+    @BeforeTest
+    public void beforeTest() {
+        System.setProperty("webdriver.ie.driver","src\\test\\resources\\IEDriverServer.exe");
+        driver= new InternetExplorerDriver();
+    }
+
+ 
+
+    @AfterTest
+    public void afterTest() {
+        //driver.close();
+        
+    }
+
+ 
+
+}
+ 
